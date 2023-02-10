804baf32130e433da4af4d6ef319535c1ace7b69
Paul Simroth <paul.simroth@proton.me>
2023-02-10 15:01:09 +0100

    content main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import styles from './page.module.css';
+import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/outline';
 
 export default function HomePage() {
   return (
-    <main>
       <div className='flex flex-col items-center justify-center h-screen px-2 text-white'>
         <h1 className='text-5xl font-bold mb-20'>AI Chat</h1>
 
-        <div>
+        <div className='flex space-x-2 text-center'>
           <div>
             <div className='flex flex-col items-center justify-center mb-5'>
-              {/**Sun Icon */}
+              <SunIcon className="h-8 w-8" />
               <h2>Examples</h2>
             </div>
 
@@ -19,8 +19,33 @@ export default function HomePage() {
               <p className='infoText'>"What is the color of the sun?"</p>
             </div>
           </div>
+
+          <div>
+            <div className='flex flex-col items-center justify-center mb-5'>
+              <BoltIcon className="h-8 w-8" />
+              <h2>Capabilities</h2>
+            </div>
+
+            <div className='space-y-2'>
+              <p className='infoText'>Change the ChatGPT Version to use</p>
+              <p className='infoText'>Messages are stored with Firebase</p>
+              <p className='infoText'>Notifications when AI is thinking</p>
+            </div>
+          </div>
+
+          <div>
+            <div className='flex flex-col items-center justify-center mb-5'>
+              <ExclamationTriangleIcon className="h-8 w-8" />
+              <h2>Limitations</h2>
+            </div>
+
+            <div className='space-y-2'>
+              <p className='infoText'>May occationally generate incorrect information</p>
+              <p className='infoText'>May occationally produce harmful instructions</p>
+              <p className='infoText'>Limited knowledge on events after 2021</p>
+            </div>
+          </div>
         </div>
       </div>
-    </main>
   )
 }
