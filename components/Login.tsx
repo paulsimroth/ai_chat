f238eef9254221b5009533b666ea1db8ad536d8b
Paul Simroth <paul.simroth@proton.me>
2023-02-13 12:34:04 +0100

    login functionality added

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,9 +4,11 @@ import Image from 'next/image';
 
 function Login() {
   return (
-    <div>
+    <div className='bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center'>
       <Image src="https://links.papareact.com/2i6" width={300} height={300} alt="logo"/>
-      <button>Sign in to use the App</button>
+      <button onClick={() => signIn('google')} className='text-white font-bold text-3xl animate-pulse'>
+        Sign In to use the App
+      </button>
     </div>
   )
 };
