ca6529ec7a887a6b3e1ba61c8bf5eed5fa253084
Paul Simroth <paul.simroth@proton.me>
2023-02-13 12:41:03 +0100

    sign out button

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,12 @@
-import NewChat from "./NewChat"
+'use client';
+
+import NewChat from './NewChat';
+import { useSession, signOut } from 'next-auth/react';
 
 function SideBar() {
+
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
         <div className="flex-1">
@@ -15,6 +21,15 @@ function SideBar() {
                 {/**Map through Chats */}
             </div>
         </div>
+
+        {session && (
+          <img
+            onClick={(() => signOut())}
+            src={session.user?.image!}
+            alt="profile picture"
+            className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50'
+            />
+        )}
     </div>
   )
 };
